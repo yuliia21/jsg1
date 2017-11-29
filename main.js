575bf7fe9683ccfc05f0560835339cf3b493d183
Yuliia <yuliia@Julias-MacBook-Pro.local>
2017-11-29 00:30:01 -0800

    task 1

---
+++ main.js
@@ -0,0 +1,17 @@
+let greeting = "Hello World";
+console.log(greeting);
+
+let a=5;
+let k=10;
+console.log(a+k);
+
+let fruit = "apples";
+let person = "James";
+console.log(person + " likes " + fruit);
+
+function fullName(name, lastName){
+    return fullName=name + " " + lastName;
+}
+console.log(fullName("Emily","Halligan"));
+
+
